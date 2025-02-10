34d9520befa130414db3412513e95162aa54715a
ChristianWork05 <christian.lanza@recargaexpress.com>
2025-02-10 19:01:19 -0400

    commit 3

--- pages/LandingPage.vue
+++ pages/LandingPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <h1 class="text-3xl font-bold">Bienvenido a la Landing Principal</h1>
+      <h1 class="text-3xl font-bold">Bienvenido a la Landing Principal puerbas</h1>
       <!-- Aquí puedes empezar a crear la estructura de tu landing principal -->
     </div>
   </template>
